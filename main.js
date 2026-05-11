2153a4450c45bc5e5736dcc2be15c7ec14269e45
SurMinus <khangnguyen15081983@gmail.com>
2026-05-11 05:00:35 +0000

    klown is a sucks client

--- main.js
+++ main.js
@@ -44,11 +44,17 @@ document.addEventListener('DOMContentLoaded', () => {
                         <div class="release-date">${date}</div>
                         <div class="release-description">${escapeHtml(description)}</div>
                         <div class="release-download">
-                            <a href="${downloadUrl}" class="download-btn primary" title="Download this release">
-                                <i class="fas fa-download"></i> Download
-                            </a>
-                            <a href="${viewUrl}" class="download-btn secondary" target="_blank" title="View on GitHub">
-                                <i class="fab fa-github"></i> View
+                            ${isLatest ? '' : `<a href="${downloadUrl}" class="download-btn primary" title="Download this release">
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                    <polyline points="7 10 12 15 17 10"></polyline>
+                                    <line x1="12" y1="15" x2="12" y2="3"></line>
+                                </svg> Download
+                            </a>`}
+                            <a href="${viewUrl}" class="download-btn ${isLatest ? 'primary' : 'secondary'}" target="_blank" title="View on GitHub">
+                                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 405 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.59-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z"/>
+                                </svg> View
                             </a>
                         </div>
                     </div>
